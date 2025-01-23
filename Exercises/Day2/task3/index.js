3a4b6a032c40455120a8430246f4b0952c905d10
PrathamZWT <pratham@zealousweb.com>
2025-01-23 11:05:11 +0530

    hello

--- Exercises/Day2/task3/index.js
+++ Exercises/Day2/task3/index.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const url = require('url');
 const fileNameArray = require("../file"); 
 const fileData = require("../task3/fileContentFetch");
+const path = require('path');
+
 
 const myServer = http.createServer((req, res) => {
     const myUrl = url.parse(req.url, true);
@@ -19,7 +21,7 @@ const myServer = http.createServer((req, res) => {
             break;
 
             case "/file":
-                const fileName = myUrl.query.name;
+                let fileName = myUrl.query.name;
             
                 if (!fileName) {
                     res.writeHead(400, { "Content-Type": "text/plain" });
@@ -27,6 +29,7 @@ const myServer = http.createServer((req, res) => {
                     return;
                 }
             
+
                 const data = fileData(fileName); // Synchronously fetch file data
             
                 if (data.startsWith("Error")) {
@@ -37,6 +40,61 @@ const myServer = http.createServer((req, res) => {
                     res.end(data); // Send the file content as plain text
                 }
                 break;
+                case "/create":
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                let filecreateName = myUrl.query.name;
+                let content = myUrl.query.content;
+                const filePath = path.join(__dirname , ".." , "allFiles", filecreateName);
+                if (fs.existsSync(filePath) === true) {
+                                res.end(" File already exists!!! \n\n\n");
+                }else{
+                    fs.writeFileSync(filePath, content);
+                                   if (fs.statSync(filePath).isFile()) {
+                                    res.end("file is create successfully") ;
+                                    return;
+                                   }else {
+
+                                res.end("file not created");
+                            }
+                                }
+                break;
+                case "/append":
+                    let appendName = myUrl.query.name;
+                    let appendContent = myUrl.query.content;
+                    const fileappendPath = path.join(__dirname , ".." , "allFiles", appendName);
+                    if (fs.existsSync(fileappendPath) !== true) {
+                        res.end(" No such file  exists!!! \n\n\n");
+                        return;
+                    }
+                    else{
+                        fs.appendFileSync(fileappendPath, appendContent);
+                        let updatedData =  fs.readFileSync(fileappendPath,"utf-8");
+                        res.end(updatedData);
+                    }
+                    break;
+                    case "/delete":
+                        let deleteName = myUrl.query.name;
+                        const filedeletePath = path.join(__dirname , ".." , "allFiles", deleteName);
+                        if (fs.existsSync(filedeletePath) !== true) {
+                            res.end("no such file exists");
+                            return;
+                        }
+                        else{
+
+                            fs.unlink(filedeletePath,(error,result)=>{
+                                if(error){
+                                    res.end("file was not deleted");
+                                    return;
+                                }else{
+                                    if (fs.existsSync(filedeletePath) !== true) {
+                                        res.end("file was deleted successfully");
+                                        return;
+                                    }
+                                }
+                            });  
+                            break
+                        }
+                            
         default:
             res.writeHead(404, { "Content-Type": "text/plain" });
             res.end("404 Not Found");
